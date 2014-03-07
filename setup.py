87d8e711e31a9234410e213ba83b2fcbda6e1001
Shawn Siefkas <shawn.siefkas@meredith.com>
2014-03-07 13:26:28 -0600

    Adding a distutils setup script

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='zabbix-apache-stats',
+    version='0.0.1',
+    author='Paulson McIntyre',
+    author_email='paul@gpmidi.net',
+    description='Zabbix Apache Stat Collector',
+    entry_points={
+        'console_scripts': ['zabbix-apache-stats=fetch:main']
+    },
+    license = 'GPLv2',
+    package_dir = {'': 'bin'},
+    py_modules = ['fetch'],
+)
+
+
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
